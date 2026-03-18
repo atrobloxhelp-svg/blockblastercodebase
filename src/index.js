c0e3c8855871d9befb878f0290a1a704f2231367
atrobloxhelp-svg <atrobloxhelp@gmail.com>
2026-03-18 09:25:04 -0700

    Replace App component with BlockBlasterSolver

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import BlockBlasterSolver from './BlockBlasterSolver';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BlockBlasterSolver />
   </React.StrictMode>
 );
